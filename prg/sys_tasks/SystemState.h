4d3fab9f21956984d466814f1f48b3fa4f31bc88
D. van de Spijker <spijker.dorus@gmail.com>
2018-04-21 19:04:31 +0200

    + sys_tasks/SystemState : Prototypes SystemState API & state callbacks.

--- prg/sys_tasks/SystemState.h
+++ prg/sys_tasks/SystemState.h
@@ -19,7 +19,45 @@
 #define SYSTEM_STATE_FAIL	GFSM_FAIL
 #define SYSTEM_STATE_ERR	GFSM_OK
 
-void SysStateInitOnEnter(FsmState_t prev_state, FsmState_t new_state);
+int SystemStateInit(void);
+int SystemStateTransition(FsmState_t new_state);
 
+/* State Callbacks. */
+
+/* Initialization state. */
+void StateInitOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StateInitOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StateInitOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StateInitOnValidate(FsmState_t curr_state, FsmState_t next_state);
+
+/* Idle state. */
+void StateIdleOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StateIdleOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StateIdleOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StateIdleOnValidate(FsmState_t curr_state, FsmState_t next_state);
+
+/* Running state, */
+void StateRunningOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StateRunningOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StateRunningOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StateRunningOnValidate(FsmState_t curr_state, FsmState_t next_state);
+
+/* Connected state. */
+void StateConnectedOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StateConnectedOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StateConnectedOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StateConnectedOnValidate(FsmState_t curr_state, FsmState_t next_state);
+
+/* Transfer state. */
+void StateTransferOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StateTransferOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StateTransferOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StateTransferOnValidate(FsmState_t curr_state, FsmState_t next_state);
+
+/* Pumping state. */
+void StatePumpingOnEnter(FsmState_t prev_state, FsmState_t curr_state);
+void StatePumpingOnExit(FsmState_t curr_state, FsmState_t next_state);
+bool StatePumpingOnGuard(FsmState_t curr_state, FsmState_t next_state);
+bool StatePumpingOnValidate(FsmState_t curr_state, FsmState_t next_state);
 
 #endif /* SYSTEM_STATE_H_ */
