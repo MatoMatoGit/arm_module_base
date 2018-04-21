1e288076a72c9f248981c54c2816968260c29559
D. van de Spijker <spijker.dorus@gmail.com>
2018-04-21 18:33:40 +0200

    + sys_tasks/SystemState : Include GFsm & SYSTEM_STATE_* return values.

--- prg/sys_tasks/SystemState.h
+++ prg/sys_tasks/SystemState.h
@@ -8,10 +8,16 @@
 #ifndef SYSTEM_STATE_H_
 #define SYSTEM_STATE_H_
 
+#include "GFsm.h"
+
 #include <stdlib.h>
 
 #define SYSTEM_STATUS_BLINK_FAST_INTERVAL_MS 100
 #define SYSTEM_STATUS_BLINK_SLOW_INTERVAL_MS 800
+
+#define SYSTEM_STATE_OK		GFSM_OK
+#define SYSTEM_STATE_FAIL	GFSM_FAIL
+#define SYSTEM_STATE_ERR	GFSM_OK
 
 void SysStateInitOnEnter(FsmState_t prev_state, FsmState_t new_state);
 
