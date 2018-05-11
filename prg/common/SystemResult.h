2ea6a119ef0934df7a8aee45f520f552b8da421b
D. van de Spijker <spijker.dorus@gmail.com>
2018-05-11 19:18:55 +0200

    + common/SystemResult

---
+++ prg/common/SystemResult.h
@@ -0,0 +1,19 @@
+/*
+ * SystemResult.h
+ *
+ *  Created on: 11 mei 2018
+ *      Author: Dorus
+ */
+
+#ifndef SYSTEM_RESULT_H_
+#define SYSTEM_RESULT_H_
+
+typedef enum {
+	SYS_RESULT_ERROR	= -2,
+	SYS_RESULT_FAIL		= -1,
+	SYS_RESULT_OK		=  0,
+	SYS_RESULT_BUSY		=  1,
+	SYS_RESULT_TIMEOUT	=  2
+}SysResult_t;
+
+#endif /* SYSTEM_RESULT_H_ */
