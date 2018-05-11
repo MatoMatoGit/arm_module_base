f2637faaf5b9847f5d703f0382b58a54e1700577
D. van de Spijker <spijker.dorus@gmail.com>
2018-05-11 19:20:33 +0200

    + drivers/Time : RTC driver.

---
+++ prg/drivers/Time/Time.h
@@ -0,0 +1,23 @@
+/*
+ * Time.h
+ *
+ *  Created on: 11 mei 2018
+ *      Author: Dorus
+ */
+
+#ifndef TIME_H_
+#define TIME_H_
+
+#include "SystemResult.h"
+
+#include "OsTypes.h"
+
+SysResult_t TimeInit(Id_t mbox_time, U8_t hour_addr);
+
+SysResult_t TimeAlarmSet(U8_t hour);
+
+void TimeAlarmEnable(U8_t val);
+
+U32_t TimeUptimeGet(void);
+
+#endif /* TIME_TIME_H_ */
