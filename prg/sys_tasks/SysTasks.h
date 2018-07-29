00d743cfe1ee180be96fb0e198977a246c5b54f2
D. van de Spijker <spijker.dorus@gmail.com>
2018-07-29 15:45:01 +0200

    - sys_tasks/SysTasks : Old init calls.

--- prg/sys_tasks/SysTasks.h
+++ prg/sys_tasks/SysTasks.h
@@ -8,19 +8,13 @@
 #ifndef SYS_TASKS_H_
 #define SYS_TASKS_H_
 
-#include "SystemState.h"
 
 SysResult_t SysTasksInit(void)
 {
 	SysResult_t res = SYS_RESULT_ERROR;
 
-	res = SysTaskPermStorageMngrInit();
-
-	if(res == SYS_RESULT_OK) {
-		res = SysTaskSystemStateInit();
-	}
 
 	return res;
 }
 
-#endif /* SYS_TASKS_SYSTASKS_H_ */
+#endif /* SYS_TASKS_H_ */
