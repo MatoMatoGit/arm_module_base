d567b18a99ee5128ee8438e3e8b89aefd5ae5eb0
D. van de Spijker <spijker.dorus@gmail.com>
2018-07-29 15:43:40 +0200

    + common/SystemResult : SYS_RESULT_INV_ARG.

--- prg/common/SystemResult.h
+++ prg/common/SystemResult.h
@@ -9,7 +9,8 @@
 #define SYSTEM_RESULT_H_
 
 typedef enum {
-	SYS_RESULT_ERROR	= -2,
+	SYS_RESULT_ERROR	= -3,
+	SYS_RESULT_INV_ARG	= -2,
 	SYS_RESULT_FAIL		= -1,
 	SYS_RESULT_OK		=  0,
 	SYS_RESULT_BUSY		=  1,
