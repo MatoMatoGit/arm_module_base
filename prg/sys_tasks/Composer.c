5949d0f6c50d1e4e4d48971a7e05387cd42b2a4f
D. van de Spijker <spijker.dorus@gmail.com>
2018-08-02 22:02:32 +0200

    + sys_tasks/Composer : Header.
    + sys_tasks/Composer : ComposerInit. 
    + sys_tasks/Composer : Check return values.

--- prg/sys_tasks/Composer.c
+++ prg/sys_tasks/Composer.c
@@ -4,7 +4,7 @@
  *  Created on: 1 aug. 2018
  *      Author: Dorus
  */
-#include "SystemResult.h"
+#include "Composer.h"
 
 #include "PriorRTOS.h"
 
@@ -17,29 +17,54 @@
 #define EVG_SYSTEM_FLAG_ERROR			0x02
 #define EVG_SYSTEM_FLAG_ERROR_CRIT		0x03
 
+SysResult_t ComposerInit(void)
+{
+	SysResult_t res = SYS_RESULT_INV_ARG;
+
+	res = SYS_RESULT_ERROR;
+
+	if(TaskCreate(TaskComposer, TASK_CAT_HIGH, 5, TASK_PARAMETER_START, 0, NULL, 0) != ID_INVALID) {
+		res = SYS_RESULT_OK;
+	}
+
+	return res;
+}
+
 static void TaskComposer(void *p_arg, U32_t v_arg)
 {
 	Id_t evg_system = ID_INVALID;
 	Id_t mbox_irrigation = ID_INVALID;
 	Id_t mbox_schedule = ID_INVALID;
 
-	SysResult_t res = SYS_RESULT_ERROR;
+	SysResult_t res = SYS_RESULT_OK;
 
 	evg_system = EventgroupCreate();
+	if(evg_system == ID_INVALID) {
+		res = SYS_RESULT_ERROR;
+	}
+
+	if(res == SYS_RESULT_OK) {
+		res = TimeInit(evg_system, EVG_SYSTEM_FLAG_ALARM);
+	}
+
+	if(res == SYS_RESULT_OK) {
+		res = IrrigationControllerInit(*mbox_irrigation);
+	}
+
+	if(res == SYS_RESULT_OK) {
+		ScheduleManagerConfig_t config = {
+			.mbox_schedule = ID_INVALID,
+			.mbox_irrigation = mbox_irrigation,
+			.evg_alarm = evg_system,
+			.evg_alarm_flag = EVG_SYSTEM_FLAG_ALARM,
+		};
+		res = ScheduleManagerInit(&config);
+		mbox_schedule = config.mbox_schedule;
+	}
 	
-	res = TimeInit(evg_system, EVG_SYSTEM_FLAG_ALARM);
+	if(res == SYS_RESULT_OK) {
+		res = UiControllerInit(mbox_irrigation, mbox_schedule, evg_system);
+	}
 
-	res = IrrigationControllerInit(*mbox_irrigation);
-
-	ScheduleManagerConfig_t config = {
-		.mbox_schedule = ID_INVALID,
-		.mbox_irrigation = mbox_irrigation,
-		.evg_alarm = evg_system,
-		.evg_alarm_flag = EVG_SYSTEM_FLAG_ALARM,
-	};
-	res = ScheduleManagerInit(&config);
-	mbox_schedule = config.mbox_schedule;
-
-	res = UiControllerInit(mbox_irrigation, mbox_schedule, evg_system);
 
 }
---
+++ prg/sys_tasks/Composer.h
@@ -0,0 +1,16 @@
+/*
+ * Composer.h
+ *
+ *  Created on: 2 aug. 2018
+ *      Author: Dorus
+ */
+
+#ifndef COMPOSER_H_
+#define COMPOSER_H_
+
+#include "SystemResult.h"
+#include <stdbool.h>
+
+SysResult_t ComposerInit(void);
+
+#endif /* COMPOSER_H_ */
