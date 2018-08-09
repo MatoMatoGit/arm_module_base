e14976e0fa8f74720926002d5705c32bca1f821d
D. van de Spijker <spijker.dorus@gmail.com>
2018-08-09 22:30:51 +0200

    + drivers/Storage : MetadataSync, FileRead & FileReadConsistent.
    * drivers/Storage : Continue implementation of split metadata/data.

--- prg/drivers/Storage/storage.c
+++ prg/drivers/Storage/storage.c
@@ -10,17 +10,19 @@
 #include "flash.h"
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define STORAGE_CONFIG_SECTOR_DATA_ADDR_END STORAGE_CONFIG_DATA_SECTOR_ADDR + STORAGE_CONFIG_DATA_SECTOR_SIZE_BYTES
-#defien STORAGE_CONFIG_SECTOR_METADATA_ADDR_END STORAGE_CONFIG_METADATA_SECTOR_ADDR + STORAGE_CONFIG_METADATA_SECTOR_SIZE_BYTES
+#define STORAGE_DATA_SECTOR_ADDR_END STORAGE_CONFIG_DATA_SECTOR_ADDR + STORAGE_CONFIG_DATA_SECTOR_SIZE_BYTES
+#define STORAGE_METADATA_SECTOR_ADDR_END STORAGE_CONFIG_METADATA_SECTOR_ADDR + STORAGE_CONFIG_METADATA_SECTOR_SIZE_BYTES
 
 #define METADATA_ADDR_IS_VALID(addr) ( \
-(addr <= STORAGE_CONFIG_METADATA_SECTOR_ADDR_END) && \
+(addr <= STORAGE_METADATA_SECTOR_ADDR_END) && \
 (addr >= STORAGE_CONFIG_METADATA_SECTOR_ADDR) \
 )
 
 #define DATA_ADDR_IS_VALID(addr) ( \
-(addr <= STORAGE_CONFIG_DATA_SECTOR_ADDR_END) && \
+(addr <= STORAGE_DATA_SECTOR_ADDR_END) && \
 (addr >= STORAGE_CONFIG_DATA_SECTOR_ADDR) \
 )
 
@@ -39,12 +41,12 @@ typedef struct {
 
 FileMetadata_t Metadata[FILE_NUM];
 uintptr_t MetadataAddress = STORAGE_CONFIG_METADATA_SECTOR_ADDR;
-#define METADATA_SIZE_BYTES sizeof(FileMetadata_t) * FILE_NUM
+#define METADATA_TOTAL_SIZE_BYTES sizeof(FileMetadata_t) * FILE_NUM
 
 static SysResult_t IMetadataStore(FileMetadata_t *metadata);
 static SysResult_t IMetadataLoad(U32_t idx);
 static SysResult_t IDataStore(uintptr_t addr, void *data, U32_t size);
-
+static SysResult_t IDataLoad(U32_t idx, void *data, U32_t size);
 
 SysResult_t StorageInit(void)
 {
@@ -62,7 +64,7 @@ SysResult_t StorageInit(void)
 
 SysResult_t StorageMetadataSync(void)
 {
-	
+	return IMetadataStore();
 }
 
 SysResult_t StorageFormat(U32_t f_sizes[FILE_NUM])
@@ -73,7 +75,7 @@ SysResult_t StorageFormat(U32_t f_sizes[FILE_NUM])
 		res = SYS_RESULT_ERROR;
 	}
 	if(res == SYS_RESULT_OK) {
-		if(flash_erase(STORAGE_CONFIG_DATA_SECTOR) != FLASH_RESULT_OK) {
+		if(flash_erase(STORAGE_CONFIG_METADATA_SECTOR) != FLASH_RESULT_OK) {
 			res = SYS_RESULT_ERROR;
 		}
 	}
@@ -83,10 +85,6 @@ SysResult_t StorageFormat(U32_t f_sizes[FILE_NUM])
 		if(res != SYS_RESULT_OK) {
 			break;
 		}
-	}
-	
-	if(res == SYS_RESULT_OK) {
-		res = IMetadataStore();
 	}
 
 	return res;
@@ -118,24 +116,41 @@ SysResult_t StorageFileWrite(File_t fd, void *data, U32_t size)
 	return res;
 }
 
-SysResult_t StorageFileRead(File_t fd, void *data, U32_t size);
+SysResult_t StorageFileRead(File_t fd, void *data, U32_t size)
+{
+	SysResult_t res = SYS_RESULT_INV_ARG;
+
+	if(data != NULL && size > 0) {
+		res = IDataLoad((U32_t)fd, data, size, true);
+	}
+
+	return res;
+}
+
+SysResult_t StorageFileReadConsistent(File_t fd, void *data, U32_t size)
+{
+	SysResult_t res = SYS_RESULT_INV_ARG;
+
+	if(data != NULL && size > 0) {
+		res = IDataLoad((U32_t)fd, data, size, false);
+	}
+
+	return res;
+}
 
 static SysResult_t IMetadataInit(U32_t idx, U32_t size)
 {
 	SysResult_t res = SYS_RESULT_OK;
 
-	/* Make space for the metadata itself. */
-	size += sizeof(FileMetadata_t);
-
 	/* If this is the first file metadata, initialize the address at the configured
 	 * base address. */
 	if(idx == 0) {
-		Metadata[idx].addr = STORAGE_CONFIG_SECTOR_ADDR;
+		Metadata[idx].addr = STORAGE_CONFIG_DATA_SECTOR_ADDR;
 	} else {
 		Metadata[idx].addr = Metadata[idx -1].addr + size;
 	}
 	/* Check if the calculated address is within bounds. */
-	if(METADATA_ADDR_IS_VALID(Metadata[idx].addr)) {
+	if(DATA_ADDR_IS_VALID(Metadata[idx].addr)) {
 		Metadata[idx].addr = 0;
 		res = SYS_RESULT_ERROR;
 	}
@@ -154,21 +169,14 @@ static SysResult_t IMetadataStore(void)
 {
 	SysResult_t res = SYS_RESULT_OK;
 	
+	MetadataAddress += METADATA_TOTAL_SIZE_BYTES;
 	if(!METADATA_ADDR_IS_VALID(MetadataAddress)) {
 		res = SYS_RESULT_ERROR;
 	}
 	if(res == SYS_RESULT_OK) {
-		if(!METADATA_ADDR_IS_VALID(MetadataAddress + METADATA_SIZE_BYTES)) {
+		if(flash_write(MetadataAddress, (void *)Metadata, METADATA_TOTAL_SIZE_BYTES) != FLASH_RESULT_OK) {
 			res = SYS_RESULT_ERROR;
 		}
-	}
-	if(res == SYS_RESULT_OK) {
-		if(flash_write(MetadataAddress, (void *)Metadata, METADATA_SIZE_BYTES) != FLASH_RESULT_OK) {
-			res = SYS_RESULT_ERROR;
-		}
-	}
-	if(res == SYS_RESULT_OK) {
-		MetadataAddress += METADATA_SIZE_BYTES;
 	}
 
 	return res;
@@ -178,23 +186,17 @@ static SysResult_t IMetadataLoad(void)
 {
 	SysResult_t res = SYS_RESULT_OK;
 	U32_t i = 0;
-	FileMetadata_t tmp_md;
-	uintptr_t next_addr = 0;
+	uintptr_t addr = MetadataAddress;
 
-	tmp_md = *((FileMetadata_t *)STORAGE_CONFIG_SECTOR_ADDR);
-	while(i < idx) {
-		next_addr = tmp_md.addr + tmp_md.size;
-		if(STORAGE_ADDR_IS_VALID(next_addr)) {
-			tmp_md = *((FileMetadata_t *)next_addr);
+	while(i < FILE_NUM) {
+		if(METADATA_ADDR_IS_VALID(addr)) {
+			Metadata[i] = *((FileMetadata_t *)addr);
+			addr += sizeof(FileMetadata_t);
 		} else {
 			res = SYS_RESULT_ERROR;
 			break;
 		}
 	};
-
-	if(res == SYS_RESULT_OK) {
-		Metadata[idx] = tmp_md;
-	}
 
 	return res;
 }
@@ -211,7 +213,7 @@ static SysResult_t IDataStore(U32_t idx, void *data, U32_t size)
 	/* Check if the address is within the file address space. */
 	if(res == SYS_RESULT_OK) {
 		addr += Metadata[idx].write_offset;
-		if(!FILE_ADDR_IS_VALID(addr)) {
+		if(!FILE_ADDR_IS_VALID(addr, idx)) {
 			res = SYS_RESULT_ERROR;
 		}
 	}
@@ -226,22 +228,22 @@ static SysResult_t IDataStore(U32_t idx, void *data, U32_t size)
 		Metadata[idx].write_offset += size;
 	}
 
-	return res;	
+	return res;
 }
 
-static SysResult_t IDataLoad(U32_t idx, void *data, U32_t size)
+static SysResult_t IDataLoad(U32_t idx, void *data, U32_t size, bool inc_offset)
 {
 	SysResult_t res = SYS_RESULT_OK;
 	uintptr_t addr = Metadata[idx].addr;
 	
 	/* Check if the address is within the storage address space. */
-	if(!STORAGE_ADDR_IS_VALID(addr)) {
+	if(!DATA_ADDR_IS_VALID(addr)) {
 		res = SYS_RESULT_ERROR;
 	}
 	/* Check if the address is within the file address space. */
 	if(res == SYS_RESULT_OK) {
 		addr += Metadata[idx].read_offset;
-		if(!FILE_ADDR_IS_VALID(addr)) {
+		if(!FILE_ADDR_IS_VALID(addr, idx)) {
 			res = SYS_RESULT_ERROR;
 		}
 	}
@@ -249,8 +251,8 @@ static SysResult_t IDataLoad(U32_t idx, void *data, U32_t size)
 	if(res == SYS_RESULT_OK) {
 		memcpy(data, (const void *)addr, size);
 	}
-	/* Increase the file offset. */
-	if(res == SYS_RESULT_OK) {
+	/* Increase the file offset if inc_offset is true. */
+	if(res == SYS_RESULT_OK && inc_offset == true) {
 		Metadata[idx].read_offset += size;
 	}
 
