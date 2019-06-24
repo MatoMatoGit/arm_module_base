f26ed9f4c1da38359e51d22bc086d5e5f5495ba6
Dorus <spijker.dorus@gmail.com>
2019-06-24 13:14:25 +0200

    * drivers/OneWire/port/stm32/ow_port_macro: Use DWT usec delay instead of inaccurate assembly delay.

--- prg/drivers/OneWire/port/stm32/ow_port_macro.h
+++ prg/drivers/OneWire/port/stm32/ow_port_macro.h
@@ -8,13 +8,8 @@
 #ifndef OW_PORT_MACRO_H_
 #define OW_PORT_MACRO_H_
 
-#define OW_PORT_WAIT_US(us) do {\
-	asm volatile (	"MOV R0,%[loops]\n\t"\
-			"1: \n\t"\
-			"SUB R0, #1\n\t"\
-			"CMP R0, #0\n\t"\
-			"BNE 1b \n\t" : : [loops] "r" (16*us) : "memory"\
-		      );\
-} while(0)
+#include "dwt_stm32_delay.h"
+
+#define OW_PORT_WAIT_US(us) DWT_Delay_us(us)
 
 #endif /* OW_PORT_MACRO_H_ */
